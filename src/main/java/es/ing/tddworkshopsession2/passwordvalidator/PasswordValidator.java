99eca3178e96bebb89cac901f3de57d1f7619e05
Manuel Regino Márquez Bonilla <manuel.regino.marquez.bonilla@ing.com>
2023-11-09 23:04:23 +0100

    Last refactor: Simplified If Statement, making positive all checks.

--- src/main/java/es/ing/tddworkshopsession2/passwordvalidator/PasswordValidator.java
+++ src/main/java/es/ing/tddworkshopsession2/passwordvalidator/PasswordValidator.java
@@ -2,34 +2,30 @@ package es.ing.tddworkshopsession2.passwordvalidator;
 
 public class PasswordValidator {
     public boolean validate(String password) {
-        if (hasNotMinimumRequiredLength(password)
-                || doesNotContainsUppercaseLetters(password)
-                || doesNotContainsLowercaseLetters(password)
-                || doesNotContainsDigits(password)
-                || doesNotContainsSpecialChars(password)) {
-            return false;
-        }
-
-        return true;
+        return hasMinimumRequiredLength(password)
+                && doesContainsUppercaseLetters(password)
+                && doesContainsLowercaseLetters(password)
+                && doesContainsDigits(password)
+                && doesContainsSpecialChars(password);
     }
 
-    private static boolean hasNotMinimumRequiredLength(String password) {
-        return password.length() < 8;
+    private static boolean hasMinimumRequiredLength(String password) {
+        return password.length() >= 8;
     }
 
-    private static boolean doesNotContainsUppercaseLetters(String password) {
-        return password.toLowerCase().equals(password);
+    private static boolean doesContainsUppercaseLetters(String password) {
+        return !password.toLowerCase().equals(password);
     }
 
-    private static boolean doesNotContainsLowercaseLetters(String password) {
-        return password.toUpperCase().equals(password);
+    private static boolean doesContainsLowercaseLetters(String password) {
+        return !password.toUpperCase().equals(password);
     }
 
-    private static boolean doesNotContainsDigits(String password) {
-        return !password.matches(".*\\d.*");
+    private static boolean doesContainsDigits(String password) {
+        return password.matches(".*\\d.*");
     }
 
-    private static boolean doesNotContainsSpecialChars(String password) {
-        return !password.matches(".*[^a-zA-Z0-9].*");
+    private static boolean doesContainsSpecialChars(String password) {
+        return password.matches(".*[^a-zA-Z0-9].*");
     }
 }
