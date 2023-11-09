6a60242abecabddac959671f2cbe685ebaca4d2c
Manuel Regino Márquez Bonilla <manuel.regino.marquez.bonilla@ing.com>
2023-11-09 18:19:00 +0100

    Second use case: min length. Red-Green strategy: Obvious implementation.

--- src/main/java/es/ing/tddworkshopsession2/passwordvalidator/PasswordValidator.java
+++ src/main/java/es/ing/tddworkshopsession2/passwordvalidator/PasswordValidator.java
@@ -2,6 +2,9 @@ package es.ing.tddworkshopsession2.passwordvalidator;
 
 public class PasswordValidator {
     public boolean validate(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
         return true;
     }
 }
--- src/test/java/es/ing/tddworkshopsession2/passwordvalidator/PasswordValidatorTest.java
+++ src/test/java/es/ing/tddworkshopsession2/passwordvalidator/PasswordValidatorTest.java
@@ -2,20 +2,21 @@ package es.ing.tddworkshopsession2.passwordvalidator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PasswordValidatorTest {
 
     /* As System Admin, I need to check if a given password is strong or not
-    *
-    * Acceptance Criteria:
-    * Password must be 8 char long or more
-    * Must include capital letters
-    * Must include lowercase letters
-    * Must include one or more digits
-    * Must include one or more special characters
-    *
-    */
+     *
+     * Acceptance Criteria:
+     * Password must be 8 char long or more
+     * Must include capital letters
+     * Must include lowercase letters
+     * Must include one or more digits
+     * Must include one or more special characters
+     *
+     */
 
     @Test
     void shouldBeValidIfMeetsAllRequirements() {
@@ -26,4 +27,15 @@ class PasswordValidatorTest {
 
         assertTrue(isValid);
     }
+
+    @Test
+    void shouldNotBeValidIfIsShorterThanEightChars() {
+        String password = "ABCdef";
+        PasswordValidator passwordValidator = new PasswordValidator();
+
+        boolean isValid = passwordValidator.validate(password);
+
+        assertFalse(isValid);
+    }
+
 }
