e9764f9375a36bed8f18532a01d16f5560a25fe6
Manuel Regino Márquez Bonilla <manuel.regino.marquez.bonilla@ing.com>
2023-11-09 18:32:28 +0100

    First refactor: simplify ifs by extracting contitions to methods

--- src/main/java/es/ing/tddworkshopsession2/passwordvalidator/PasswordValidator.java
+++ src/main/java/es/ing/tddworkshopsession2/passwordvalidator/PasswordValidator.java
@@ -2,15 +2,24 @@ package es.ing.tddworkshopsession2.passwordvalidator;
 
 public class PasswordValidator {
     public boolean validate(String password) {
-        if (password.length() < 8) {
+        if (hasMinimumRequiredLength(password)
+                || containsUppercaseLetters(password)
+                || containsLowercaseLetters(password)) {
             return false;
         }
-        if (password.toLowerCase().equals(password)){
-            return false;
-        }
-        if (password.toUpperCase().equals(password)){
-            return false;
-        }
+
         return true;
+    }
+
+    private static boolean hasMinimumRequiredLength(String password) {
+        return password.length() < 8;
+    }
+
+    private static boolean containsUppercaseLetters(String password) {
+        return password.toLowerCase().equals(password);
+    }
+
+    private static boolean containsLowercaseLetters(String password) {
+        return password.toUpperCase().equals(password);
     }
 }
